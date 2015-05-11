f2de7205f31e7838ce810a3a55a24d50229b24e1
helwood <helwood245@gmail.com>
2015-05-10 22:30:41 -0400

    Expanding Overlay Addition
    Added in animation for an overlay that expands from the box’s
    coordinates to full screen.  Close functionality not working yet.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var colNum = 1,
+$(function(){
+	var colNum = 1,
 	findNext = true;
 
-$(function(){
-	$('.item').each(function() {
+	$('.project').each(function() {
 	
 		$(this).attr("data-col",colNum);
 	
@@ -14,14 +14,11 @@ $(function(){
 		
 		// Change Title of Items to Represent how many columns it is
 		if($(this).hasClass("width2")) {
-			$(this).find("h3").text("2 Col");
 			if(colNum < 4) {
 				colNum++;
 			} else {
 				colNum = 1;
 			}
-		} else {
-			$(this).find("h3").text("1 Col");
 		}
 
 		//If the item is too big to fit in the last space - rearrange
@@ -39,8 +36,61 @@ $(function(){
 					next = $(next).next();
 					findNext = true;
 				}
+				//console.log(findNext);
 			}
+			findNext = true;
 		}
+		/*var position = $(this).offset();
+		var win_width = $(window).width();
+		var item_width = $(this).width();
+		var win_height = $(window).height();
+		var item_height = $(this).height();
+		var top = position.top;
+		var left = position.left;
+		var right = (win_width - item_width) - 2;
+		var bottom = (win_height - item_height) - top - 2;
+		var css_string = "inset(" + top + "px " + right + "px " + bottom + "px " + left + "px)";
+
+		console.log(win_width);
+		//console.log(css_string);
+		//console.log("Top: " + position.top + " Right: " + right + " Left: " + position.left + " Bottom: " + bottom);
+		$(this).find('.project-overlay').css({
+			'-webkit-clip-path':'inset(0 0 0 0)',
+			'clip-path':css_string,
+			'opacity':'1',
+		});*/
 	});
-	$('.item').removeAttr("data-col");
+	$('.project').removeAttr("data-col");
+
+});
+
+
+
+$('.project').click(function(){
+		//console.log(css_string);
+		//console.log("Top: " + position.top + " Right: " + right + " Left: " + position.left + " Bottom: " + bottom);
+
+		var position = $(this).offset();
+		var win_width = $(window).width();
+		var item_width = $(this).width();
+		var win_height = $(window).height();
+		var item_height = $(this).height();
+		var top = position.top;
+		var left = position.left;
+		var right = (win_width - item_width) - 2;
+		var bottom = (win_height - item_height) - top - 2;
+		var css_string = "inset(" + top + "px " + right + "px " + bottom + "px " + left + "px)";
+		$(this).find('.project-overlay').css('left',left);
+
+		$(this).find('.project-overlay').css({
+			'-webkit-clip-path':css_string,
+			'z-index':'9999',
+			'opacity':'1',
+		}).delay(400).queue(function(){
+			$(this).css({
+				'-webkit-clip-path':'inset(0 0 0 0)',
+				'left':'0'
+			});
+		});
+
 });
